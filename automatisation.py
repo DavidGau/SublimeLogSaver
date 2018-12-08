211e11532727fe2a9a0997694cbb6b6c65f7ff17
DavidGau <gaulindavidweb@gmail.com>
2018-12-07 21:07:11 -0500

    Added notifications

--- automatisation.py
+++ automatisation.py
@@ -1,41 +1,52 @@
 """
-	Fonction permettant de sauvegarder le Build Output dans un fichier txt.
+	Function that saves the sublime build output into a txt file.
+	Requirements: Having the build output on another window
 """
 from pywinauto.application import Application
 from pywinauto import keyboard
 import pyperclip
+from win10toast import ToastNotifier as Notifier
+import time
 
+def developpment(save_dir,filename,minimize_after_op=False,notification=False):
 
-def developpment(save_dir,filename,minimize_after_op):
+	notif = Notifier()
+
+	if notification: #If notifications are enabled, warn the user
+		notif.show_toast("Log saving in 2 seconds","Please stop typing/clicking")
+		time.sleep(2)
+
 	
-	current_window = ""
 	window_found = False
 
 	app = Application().connect(path=r"C:\Program Files (x86)\Sublime Text 3\sublime_text.exe")
 	win = app.window(title_re=".*Build output*.")
 
-	first_window = str(app.windows()[0])
-	
+	log_window = str(app.windows()[0])
+
 	app_dialog = app.top_window()
 	app_dialog.minimize()
 	app_dialog.restore()
 
-	#Boucle sur tout les tabs pour trouver la bonne
-	while current_window != first_window:
 
-		keyboard.SendKeys('^{PGDN}') #Si pas directement sur la fenetre build output
-		current_window = str(app.windows()[0]) #Nom de la window
+	current_window = ""
+	
+	#Passes on every tabs until it finds it
+	while current_window != log_window:
+		keyboard.SendKeys('^{PGDN}') #Changing tab
+		current_window = str(app.windows()[0]) #Get window's name
 
 		if current_window.find("Build output") > -1:
 			window_found = True
 			break
 
 
-	#Enregistre le tout dans un txt
+	#Save into a txt file
 	if window_found:
-		win.menu_select("Selection->Select All")	#Sélectionne tout le texte du output
-		keyboard.SendKeys('^C')						#Copie le contenu
-		text_to_file = pyperclip.paste()			#Contenu du copie dans la variable
+		win.menu_select("Selection->Select All")	#Selecting the output's text
+		keyboard.SendKeys("^A")
+		keyboard.SendKeys("^C")						#Copy everything
+		text_to_file = pyperclip.paste()			#Clipboard's content to variable
 		
 		#Saving it into a txt file
 		new_file = open(save_dir +"\\"+ filename + ".txt","w")
@@ -44,5 +55,7 @@ def developpment(save_dir,filename,minimize_after_op):
 
 		if minimize_after_op:
 			app_dialog.minimize()
-		
+
+		if notification:
+			notif.show_toast("Log saved","Go on and have fun!")
 
--- testingit.py
+++ testingit.py
@@ -4,9 +4,9 @@ import time
 
 time.sleep(2)
 i = 0
-while i < 5:
+while i < 1:
 	print("Test de l'enregistreur de logs")
 	print(datetime.datetime.now())
 
-	developpment("C:\\Users\\Dave\\Desktop\\testingit","hg{0}".format(i),False)
+	developpment("C:\\Users\\Dave\\Desktop\\testingit","hdg{0}".format(i),False,True)
 	i += 1
